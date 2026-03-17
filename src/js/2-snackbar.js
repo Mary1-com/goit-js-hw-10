7a46907e7ce0e67d6f40dcb3f0b118cb809deb7d
Mary1-com <vnnkvmrn76@gmail.com>
2026-03-17 11:25:27 +0200

    First commit HW 10

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,35 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault;
+
+    const delay = Number(form.elements.delay.value);
+    const state = form.elements.delay.value;
+
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            state === 'fulfield' ? resolve(delay) : reject(delay);
+        }, delay);
+    });
+
+    promise
+        .then(delay => {
+            iziToast.success({
+                message: `✅ Fulfilled promise in ${delay}ms`,
+                position: "topRight",
+            });
+        })
+        catch (delay => {
+        iziToast.error({
+            message: `❌ Rejected promise in ${delay}ms`,
+            position: "topRight",
+        });
+    });
+
+form.reset();
+});
---
+++ src/main.js
@@ -0,0 +1,2 @@
+
+import "./js/1-timer.js";
